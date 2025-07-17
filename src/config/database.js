a457a5baa04c616caf490d87b4bcd3240234a585
Rsls12 <rlucensama@gmail.com>
2025-07-16 23:42:05 -0500

    2intento

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-// Verifica que la variable de entorno se esté leyendo correctamente
-const databaseUrl = process.env.DATABASE_URL;
+dotenv.config();  // Asegúrate de cargar las variables de entorno
+
+const databaseUrl = process.env.POSTGRES_URL;
 
 if (!databaseUrl) {
-  throw new Error('La variable de entorno DATABASE_URL no está configurada.');
+  throw new Error('La variable de entorno POSTGRES_URL no está configurada.');
 }
 
 const sequelize = new Sequelize(databaseUrl, {
@@ -16,5 +18,14 @@ const sequelize = new Sequelize(databaseUrl, {
     },
   },
 });
+
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos exitosa');
+  })
+  .catch((err) => {
+    console.error('Error de conexión a la base de datos:', err);
+  });
 
 export default sequelize;
